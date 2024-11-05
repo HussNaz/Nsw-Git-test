8407da6cb1616fd3cb58cd13fff07c80b2e307dc
HussNaz <nazmulhassanc09@gmail.com>
2024-11-05 11:13:54 +0600

    Commit from Nazmul

---
+++ test2/testProjectFromNazmul/src/Nazmul.java
@@ -0,0 +1,8 @@
+public class Nazmul {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world from Nazmul!");
+        System.out.println("Devit!! you are awesome");
+        System.out.println("Devit!! Love from Nazmul");
+    }
+}
