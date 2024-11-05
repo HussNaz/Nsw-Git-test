773b4e24d887bbee039097cad3d5807da3dac007
HussNaz <nazmulhassanc09@gmail.com>
2024-11-05 12:27:59 +0600

    commit for task-2

--- test2/testProjectFromNazmul/src/Nazmul.java
+++ test2/testProjectFromNazmul/src/Nazmul.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Nazmul {
     public static void main(String[] args) {
 
         System.out.println("Hello world from Nazmul!");
         System.out.println("Devit!! you are awesome");
         System.out.println("Devit!! Love from Nazmul");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
     }
 }
